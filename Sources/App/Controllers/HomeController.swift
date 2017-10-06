3a9483172f77ca4a936e7451dde22727c53039f3
Noah Fleischmann <noahfleischmann21@gmail.com>
2017-10-06 20:07:24 +0200

    Small improvements

--- Sources/App/Controllers/HomeController.swift
+++ Sources/App/Controllers/HomeController.swift
@@ -25,25 +25,25 @@ final class HomeController {
         let featuredAccessoryImage: String
         
         // Limit of fetched items for accessories query
-        let visibleAccessoriesLimit = 20
+        let visibleAccessoriesLimit = 18
 
         // Get limited amout of accessoires sorted by date
         accessories = try Accessory.makeQuery().filter("approved", true).limit(visibleAccessoriesLimit).all().sorted(by: { $0.date.compare($1.date) == .orderedDescending })
 
         // Get featured accessory from item id
-        let featuredAccessoryDbId = 26
+        let featuredAccessoryId = 26
         // Path to the banner image of the featured image
         featuredAccessoryImage = "/images/featured-item.png"
         
         // Fetch featured accessory
-        featuredAccessory = try Accessory.makeQuery().filter("id", featuredAccessoryDbId).first()!
+        featuredAccessory = try Accessory.makeQuery().filter("id", featuredAccessoryId).first()!
 
         
         let nodes = try Node(node: [
             "categories": categories.makeNode(in: nil),
             "accessories": accessories.makeNode(in: nil),
             "featuredImage": featuredAccessoryImage.makeNode(in: nil),
-            "featuredProductLink": featuredAccessory.productLink.makeNode(in: nil),
+            //"featuredAccessory": featuredAccessory.productLink.makeNode(in: nil),
             "noAccessories": accessories.count == 0
             ])
         return try droplet.view.make("home", nodes)
