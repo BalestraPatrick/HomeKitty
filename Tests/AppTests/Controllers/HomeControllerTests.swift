c9fe10fcfd80425de593ac773892ff813535d48c
Noah Fleischmann <noahfleischmann21@gmail.com>
2017-10-07 09:22:33 +0200

    Add HomeControllerTests

---
+++ Tests/AppTests/Controllers/HomeControllerTests.swift
@@ -0,0 +1,26 @@
+//
+//  Copyright © 2017 Patrick Balestra. All rights reserved.
+//
+
+import XCTest
+import Testing
+@testable import Vapor
+@testable import App
+
+class HomeControllerTests: TestCase {
+    
+    static var allTests : [(String, (HomeControllerTests) -> () throws -> Void)] {
+        return [
+            ("testHome", testHome),
+        ]
+    }
+    
+    let drop = try! Droplet.testable()
+    
+    func testHome() throws {
+        try drop
+            .testResponse(to: .get, at: "")
+            .assertStatus(is: .ok)
+    }
+}
+
