d069190861203fefd5ee261697b21822be895315
Noah Fleischmann <noahfleischmann21@gmail.com>
2017-10-07 12:45:26 +0200

    Time ago string

---
+++ Sources/App/Extensions/Date+Utils.swift
@@ -0,0 +1,47 @@
+//
+//  Copyright © 2017 Patrick Balestra. All rights reserved.
+//
+
+import Vapor
+
+extension Date {
+    func timeAgoString() -> String {
+        let secondsAgo = Int(Date().timeIntervalSince(self))
+        
+        let minute = 60
+        let hour = 60 * minute
+        let day = 24 * hour
+        let week = 7 * day
+        let month = 4 * week
+        let year = 12 * month
+        
+        let quotient: Int
+        let unit: String
+        
+        if secondsAgo < minute {
+            quotient = secondsAgo
+            unit = "second"
+        } else if secondsAgo < hour {
+            quotient = secondsAgo / minute
+            unit = "min"
+        } else if secondsAgo < day {
+            quotient = secondsAgo / hour
+            unit = "hour"
+        } else if secondsAgo < week {
+            quotient = secondsAgo / day
+            unit = "day"
+        } else if secondsAgo < month {
+            quotient = secondsAgo / week
+            unit = "week"
+        } else if secondsAgo < year {
+            quotient = secondsAgo / month
+            unit = "month"
+        } else {
+            quotient = secondsAgo / year
+            unit = "year"
+        }
+        
+        return "\(quotient) \(unit)\(quotient == 1 ? "" : "s") ago"
+    }
+}
+
