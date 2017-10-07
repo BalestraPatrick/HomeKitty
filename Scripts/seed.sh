de070d11aaa746add9c183e5e374920005519d92
Viktor Sokolov <bntrsokolov@gmail.com>
2017-10-07 11:25:11 +0300

    add ability to display icon which indicates is accessory require hub

--- Scripts/seed.sh
+++ Scripts/seed.sh
@@ -3,6 +3,8 @@
 set -e
 psql -U "$DB_USER" -d "$DB_DATABASE" -p 5432 -a -f seed_categories.sql || (echo "🚨  Failed seed categories"; exit 1;)
 psql -U "$DB_USER" -d "$DB_DATABASE" -p 5432 -a -f seed_manufacturers.sql || (echo "🚨  Failed seed manufacturers"; exit 1;)
+psql -U "$DB_USER" -d "$DB_DATABASE" -p 5432 -a -f seed_bridges.sql || (echo "🚨  Failed seed bridges"; exit 1;)
 psql -U "$DB_USER" -d "$DB_DATABASE" -p 5432 -a -f seed_lights.sql || (echo "🚨  Failed seed lights"; exit 1;)
 psql -U "$DB_USER" -d "$DB_DATABASE" -p 5432 -a -f seed_outlets.sql || (echo "🚨  Failed seed outlets"; exit 1;)
+
 echo "✅ Seed completed ✅"
