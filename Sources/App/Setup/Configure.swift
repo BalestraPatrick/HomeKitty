629c1fe27b569b1fb094637dbf89f34296ad87bc
Kim de Vos <kdv@trifork.com>
2018-04-02 20:12:13 +0200

    Added environment variables

--- Sources/App/Setup/Configure.swift
+++ Sources/App/Setup/Configure.swift
@@ -7,24 +7,30 @@ import FluentPostgreSQL
 import Leaf
 //import LeafErrorMiddleware
 import Stripe
-//import SendGridProvider
+import SendGrid
 
-public func configure(_ config: inout Config, env: inout Environment, services: inout Services
-    ) throws {
+public func configure(_ config: inout Config, env: inout Environment, services: inout Services) throws {
+    guard let sendGridKey = ProcessInfo.processInfo.environment["SENDGRID_API_KEY"]?.stringValue,
+        let stripeKey = ProcessInfo.processInfo.environment["STRIPE_API_KEY"]?.stringValue,
+        let dbHostname = ProcessInfo.processInfo.environment["DB_HOSTNAME"]?.stringValue,
+        let dbUsername = ProcessInfo.processInfo.environment["DB_USER"]?.stringValue,
+        let db = ProcessInfo.processInfo.environment["DB_DATABASE"]?.stringValue else {
+            fatalError("Missing some API keys")
+    }
 
+    let dbPort = ProcessInfo.processInfo.environment["DB_PORT"]?.intValue ?? 5432
+    let dbPassword = ProcessInfo.processInfo.environment["DB_PASSWORD"]?.stringValue
 
     // Register providers first
     try services.register(FluentPostgreSQLProvider())
     try services.register(LeafProvider())
 
     // Stripe
-    //    let config = StripeConfig(apiKey: "sk_12345678")
-    //
-    //    services.register(config)
-    //
-    //    StripeProvider()
-    //
-    //    try services.register()
+    let config = SendGridConfig(apiKey: sendGridKey)
+
+    services.register(config)
+
+    try services.register(SendGridProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -39,7 +45,8 @@ public func configure(_ config: inout Config, env: inout Environment, services: 
     services.register(middlewares)
 
     // Configure a SQLite database
-    let databases = PostgreSQLDatabase(config: PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "kimdevos", database: "postgres"))
+
+    let databases = PostgreSQLDatabase(config: PostgreSQLDatabaseConfig(hostname: dbHostname, port: dbPort, username: dbUsername, database: db, password: dbPassword))
     databases.enableLogging(using: DatabaseLogger.print)
     
     services.register(databases)
