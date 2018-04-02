0690339cac580003cee8a4583c9ec75abd4e1c17
Kim de Vos <kdv@trifork.com>
2018-04-02 19:32:15 +0200

    Added missing js to contribute js

--- Public/js/contribute.js
+++ Public/js/contribute.js
@@ -5,4 +5,41 @@ function toggleBridgesSelectionAppearance() {
     } else {
         bridgeOptionsList.style.display = 'block';
     }
+}
+
+function showHide() {
+    var addNewButton = document.getElementById('add-new-button');
+    var form = document.getElementById('add-manufacturer-form');
+    var optionList = document.getElementById('manufacturer-selection');
+    if (form.style.display !== 'none') {
+        form.style.display = 'none';
+        addNewButton.innerHTML = 'Add New Manufacturer';
+        optionList.required = true;
+        optionList.disabled = false;
+        document.getElementById('manufacturer-name-input').required = false;
+        document.getElementById('manufacturer-name-input').disabled = true;
+        document.getElementById('manufacturer-logo-input').required = false;
+        document.getElementById('manufacturer-logo-input').disabled = true;
+        document.getElementById('manufacturer-website-input').required = false;
+        document.getElementById('manufacturer-website-input').disabled = true;
+    } else {
+        form.style.display = 'block';
+        addNewButton.innerHTML = 'Choose Existing';
+        optionList.required = false;
+        optionList.disabled = true;
+        document.getElementById('manufacturer-name-input').required = true;
+        document.getElementById('manufacturer-name-input').disabled = false;
+        document.getElementById('manufacturer-logo-input').required = true;
+        document.getElementById('manufacturer-logo-input').disabled = false;
+        document.getElementById('manufacturer-website-input').required = true;
+        document.getElementById('manufacturer-website-input').disabled = false;
+    }
+}
+
+function formatPriceInput() {
+    var priceInput = document.getElementById('price-input');
+    if (priceInput) {
+        var formattedPrice = Number(priceInput.value).toFixed(2);
+        priceInput.value = isNaN(formattedPrice) ? "" : formattedPrice;
+    }
     }
