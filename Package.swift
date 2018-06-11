48fe004a363af67462c20a28840321eb34968d7f
Kim de Vos <kdv@trifork.com>
2018-06-11 20:31:33 +0200

    Updated Vapor from RC to final release

--- Package.swift
+++ Package.swift
@@ -9,14 +9,14 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.4"),
         .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0-rc"),
 
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.0.9"),
 //        .package(url: "https://github.com/brokenhandsio/leaf-error-middleware.git", from: "0.1.0"),
-        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.4")
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor",
