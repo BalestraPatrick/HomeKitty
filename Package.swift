cae5b0ebfea86f343ebe70d26b7076f986104478
Kim de Vos <kdv@trifork.com>
2018-06-23 16:04:22 +0200

    Added FluentQuery package

--- Package.swift
+++ Package.swift
@@ -16,22 +16,26 @@ let package = Package(
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.0.9"),
 //        .package(url: "https://github.com/brokenhandsio/leaf-error-middleware.git", from: "0.1.0"),
-        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.4")
+        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.4"),
+
+        // More advanced postgres queries. Might be implemented PostgresFluent later
+        .package(url: "https://github.com/MihaelIsaev/FluentQuery.git", from: "0.4.2")
     ],
     targets: [
-        .target(name: "App", dependencies: ["Vapor",
-                                            "Fluent",
-                                            "FluentPostgreSQL",
-                                            "Leaf",
-                                            "Stripe",
-                                            "SendGrid"],
-                exclude: [
-                    "Config",
-                    "Database",
-                    "Localization",
-                    "Public",
-                    "Resources",
-                    ]),
+        .target(name: "App",
+                dependencies: ["Vapor",
+                               "Fluent",
+                               "FluentPostgreSQL",
+                               "Leaf",
+                               "Stripe",
+                               "SendGrid",
+                               "FluentQuery"],
+                exclude: ["Config",
+                          "Database",
+                          "Localization",
+                          "Public",
+                          "Resources"]
+        ),
         .testTarget(name: "AppTests", dependencies: ["App"]),
         .target(name: "Run", dependencies: ["App"])
     ]
