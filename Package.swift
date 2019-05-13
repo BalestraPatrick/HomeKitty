2ac37fb38099a8617fdeb99daff7eb9d06b3e72b
Kim de Vos <kimdevos12@hotmail.com>
2019-05-13 20:00:27 +0200

    Set min version in package.swift

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
         .executable(name: "Run", targets: ["Run"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.7"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "3.0.0"),
-
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "3.2.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
-        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
-        .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.0.9"),
-        .package(url: "https://github.com/brokenhandsio/leaf-error-middleware.git", .branch("vapor3")),
-        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.4"),
-        .package(url: "https://github.com/vapor/database-kit.git", from: "1.3.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.2"),
+        .package(url: "https://github.com/vapor-community/stripe-provider.git", from: "2.3.2"),
+        .package(url: "https://github.com/brokenhandsio/leaf-error-middleware.git", from: "1.1.0"),
+        .package(url: "https://github.com/vapor-community/sendgrid-provider.git", from: "3.0.6"),
+        .package(url: "https://github.com/vapor/database-kit.git", from: "1.3.3"),
     ],
     targets: [
         .target(name: "App",
